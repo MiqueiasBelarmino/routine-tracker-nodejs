86a9d0a310b8cd21267755a56b9008945cb80942
Miquéias Belarmino <miqueiascostabac@gmail.com>
2023-01-21 00:46:32 -0300

    build: add cors and prisma/client dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from '@prisma/client';
 
 const app = fastify();
+const prisma = new PrismaClient();
+
+app.register(cors);
 
 app.get('/', () => {
     return 'working'
+})
+
+app.get('/habits', async () => {
+    const habits = await prisma.habit.findMany();
+    return habits;
 })
 
 app.listen({port: 3333}).then(() => { console.log('Server running') })
