fd26fa4553e1489c605801bba009f6440307d90e
Miquéias Belarmino <miqueiascostabac@gmail.com>
2023-01-21 11:34:34 -0300

    feat: add relationships and migrations

--- prisma/migrations/20230121133537_create_table_tasks/migration.sql
+++ prisma/migrations/20230121133537_create_table_tasks/migration.sql
@@ -1,10 +1,8 @@
 -- CreateTable
-CREATE TABLE "habits" (
+CREATE TABLE "tasks" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "target_date" DATETIME NOT NULL,
-    "recurring" TEXT NOT NULL,
-    "schedule" TEXT NOT NULL,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "is_completed" BOOLEAN NOT NULL DEFAULT false
 );
