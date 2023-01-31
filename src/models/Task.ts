d97c1d92e9200965f7876a99b133187ab3d70fd0
Miquéias Belarmino <miqueiascostabac@gmail.com>
2023-01-31 14:37:43 -0300

    feat: add 'task' model

---
+++ src/models/Task.ts
@@ -0,0 +1,10 @@
+import { Task as TaskSchema } from "@prisma/client";
+
+export class Task implements TaskSchema {
+    id!: string;
+    name!: string;
+    target_date!: Date; 
+    priority!: number; 
+    createdAt!: Date; 
+    isCompleted!: boolean;
+}
