68fc8756337cf50a19b36b7e4f6e3d3e1e7e5cac
Miquéias Belarmino <miqueiascostabac@gmail.com>
2023-01-31 16:08:46 -0300

    feat: add 'task' controller and service

---
+++ src/controllers/TaskController.ts
@@ -0,0 +1,129 @@
+import { TaskService } from '../services/TaskService';
+import { z } from 'zod';
+import { Request, Response } from 'express';
+import { dateToMidnightISODate } from '../utils/helpers';
+
+export class TaskController {
+
+    static habitService = new TaskService();
+
+    create = async (req: Request, res: Response) => {
+        const createTaskParams = z.object({
+            name: z.string(),
+            target_date: z.coerce.date(),
+            priority: z.number().min(0).max(3),
+        })
+
+        
+        try {
+            const task = createTaskParams.parse(req.body);
+            console.log(task)
+            const createdTask = await new TaskService().create(task);
+            res.json({ data: createdTask });
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    }
+
+    findAll = async (req: Request, res: Response) => {
+
+        try {
+            const tasks = await new TaskService().findAll();
+            res.json({ data: tasks });
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    }
+
+    findById = async (req: Request, res: Response) => {
+
+        const params = z.object({
+            id: z.string().uuid()
+        })
+
+        try {
+            const { id } = params.parse(req.params);
+            const task = await new TaskService().findById(id);
+            res.json({ data: task });
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    }
+
+    findByDay = async (req: Request, res: Response) => {
+
+        const dayParams = z.object({
+            date: z.coerce.date()
+        })
+
+        try {
+            const { date } = dayParams.parse(req.body);
+            const tasks = await new TaskService().findByDay(date);
+            res.json({ data: tasks });
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    }
+
+    update = async (req: Request, res: Response) => {
+        const updateTaskBody = z.object({
+            name: z.string(),
+            target_date: z.coerce.date(),
+            priority: z.number().min(0).max(3),
+            isCompleted: z.boolean(),
+        })
+
+        const updateTaskParams = z.object({
+            id: z.string().uuid()
+        })
+
+        try {
+            const { id } = updateTaskParams.parse(req.params);
+            const task = updateTaskBody.parse(req.body);
+            const updatedTask = await new TaskService().update(id, task);
+            res.json({ data: updatedTask });
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    }
+
+    delete = async (req: Request, res: Response) => {
+
+        const deleteTaskParams = z.object({
+            id: z.string().uuid()
+        })
+
+        try {
+            const { id } = deleteTaskParams.parse(req.params);
+            const deletedTask = await new TaskService().delete(id);
+            if(deletedTask.statusCode){
+                res.status(deletedTask.statusCode).json({ data: {message: deletedTask.message } });
+            }else {
+                res.json({ data: deletedTask });
+            }
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    }
+
+    toggle = async (req: Request, res: Response) => {
+
+        const toggleTaskParams = z.object({
+            id: z.string().uuid(),
+        })
+
+        const toggleTaskBody = z.object({
+            date: z.coerce.date()
+        })
+
+        try {
+            const { id } = toggleTaskParams.parse(req.params);
+            const { date } = toggleTaskBody.parse(req.body);
+            const task = await new TaskService().toggle(id, date);
+            res.json({ data: { toggleTo: task }});
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    }
+
+}
---
+++ src/services/TaskService.ts
@@ -0,0 +1,118 @@
+import { Task } from './../models/Task';
+import { dateToMidnightISODate } from "../utils/helpers";
+import { PrismaClientCon } from "../lib/prisma"
+
+export class TaskService {
+
+    static prisma = PrismaClientCon;
+
+    create = async (task: Partial<Task>) => {
+
+        const createdTask = await TaskService.prisma.task.create({
+            data: {
+                name: task.name!,
+                target_date: task.target_date || dateToMidnightISODate(new Date()),
+                priority: Number(task.priority),
+                isCompleted: task.isCompleted || false,
+                createdAt: dateToMidnightISODate(new Date())
+            }
+        });
+        return { createdTask };
+    }
+
+    findAll = async () => {
+        const availableTasks = await TaskService.prisma.task.findMany();
+        return { availableTasks };
+    }
+
+    findById = async (id: string) => {
+        const task = await TaskService.prisma.task.findUnique({
+            where: {
+                id: id
+            }
+        });
+
+        return { task }
+    }
+
+    findByDay = async (date: Date) => {
+        const parsedDate = dateToMidnightISODate(date);
+
+        const availableTasks = await TaskService.prisma.task.findMany({
+            where: {
+                createdAt: parsedDate,
+            }
+        });
+
+        return { availableTasks }
+    }
+
+    update = async (id: string, task: Partial<Task>) => {
+
+        const updatedTask = await TaskService.prisma.task.update({
+            data: {
+                name: task.name!,
+                target_date: task.target_date || dateToMidnightISODate(new Date()),
+                priority: task.priority || 3,
+                isCompleted: task.isCompleted || false,
+                createdAt: dateToMidnightISODate(new Date())
+            },
+            where: {
+                id: id
+            }
+        });
+        return { updatedTask };
+    }
+
+    delete = async (id: string) => {
+        const task = await TaskService.prisma.task.findUnique({
+            where: {
+                id: id
+            }
+        });
+
+        if(task){
+            const deletedTask = await TaskService.prisma.task.delete({
+                where: {
+                    id: id
+                }
+            });
+
+            return { deletedTask };
+        }else{
+            return { statusCode: 404, message: "Task not found"}
+        }
+
+    }
+
+    toggle = async (id: string, date: Date) => {
+
+        let task = await TaskService.prisma.task.findUnique({
+            where: {
+                id: id
+            }
+        });
+
+        if (task) {
+            let isCompleted = false;
+
+            if (!task.isCompleted) {
+                isCompleted = true;
+            }
+
+            await TaskService.prisma.task.update({
+                data: {
+                    isCompleted: true
+                },
+                where: {
+                    id: task.id
+                }
+            })
+
+            return isCompleted?"checked":"unchecked";
+
+        }
+    }
+
+
+}
