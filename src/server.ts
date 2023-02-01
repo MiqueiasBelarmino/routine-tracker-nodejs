cb75a1a27e3dad890e1effd9c2c78af9444418b9
Miquéias Belarmino <miqueiascostabac@gmail.com>
2023-02-01 15:59:41 -0300

    docs: add docs to get and post habits

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import habitsRouter from "./routes/habitRoutes";
 import taskRouter from "./routes/taskRoutes";
+import swaggerUi from "swagger-ui-express";
+import swaggerConfig from './swagger.json';
 
 const app = express();
 const PORT = process.env.PORT;
@@ -11,6 +13,7 @@ app.use(cors({
     origin: '*'
 }));
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 app.use('/api/v1', habitsRouter);
 app.use('/api/v1', taskRouter);
 
