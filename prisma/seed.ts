65ea3c7a95653163b6f0c40d41140278a5d0669a
Miquéias Belarmino <miqueiascostabac@gmail.com>
2023-05-23 20:36:57 -0300

    feat: add user relations to db models

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcryptjs';
 import { PrismaClientCon as prisma } from '../src/lib/prisma';
 import { SCHEDULE } from '../src/utils/consts';
 import { addDays, dateToMidnightISODate } from '../src/utils/helpers';
@@ -9,15 +10,32 @@ async function main() {
   await prisma.day.deleteMany();
   await prisma.habit.deleteMany();
   await prisma.task.deleteMany();
+  await prisma.user.deleteMany();
 
   const firstHabitId = '068ba380-decb-496d-b762-58fefea6569c';
   const secondHabitId = '2357c721-5479-4c6d-9c2f-1dae308fa89d';
   const thirdHabitId = '704f8036-8563-4208-9150-22d14f48d252';
+  const testUserId = "6ff9b197-19c2-4d9a-8067-fa57191c7eb8";
+
+  const passwordHash = await hash("testuser", 10);
+
+  await Promise.all([
+    prisma.user.create({
+      data: {
+        id: testUserId,
+        name: 'Test User',
+        password: passwordHash,
+        username: "testuser",
+        createdAt: new Date('2023-01-15T00:00:00.000Z'),
+      }
+    }),
+  ]);
 
   await Promise.all([
     prisma.habit.create({
       data: {
         id: firstHabitId,
+        user_id: testUserId,
         name: 'Take a 10-15 minute walk in the afternoon',
         createdAt: new Date('2023-01-15T00:00:00.000Z'),
         schedule: SCHEDULE.MONING,
@@ -35,6 +53,7 @@ async function main() {
     prisma.habit.create({
       data: {
         id: secondHabitId,
+        user_id: testUserId,
         name: 'Read for 30 minutes before bed',
         createdAt: new Date('2023-01-16T00:00:00.000Z'),
         schedule: SCHEDULE.NIGHT,
@@ -54,6 +73,7 @@ async function main() {
     prisma.habit.create({
       data: {
         id: thirdHabitId,
+        user_id: testUserId,
         name: 'Take out the trash',
         createdAt: dateToMidnightISODate(new Date()),
         schedule: SCHEDULE.NIGHT,
@@ -68,6 +88,7 @@ async function main() {
     }),
     prisma.habit.create({
       data: {
+        user_id: testUserId,
         name: 'Make your bed in the morning',
         createdAt: new Date('2023-01-15T00:00:00.000Z'),
         schedule: SCHEDULE.MONING,
@@ -86,6 +107,7 @@ async function main() {
     }),
     prisma.habit.create({
       data: {
+        user_id: testUserId,
         name: 'Do a load of laundry',
         createdAt: new Date('2023-01-15T00:00:00.000Z'),
         schedule: SCHEDULE.MONING,
@@ -99,6 +121,7 @@ async function main() {
     }),
     prisma.habit.create({
       data: {
+        user_id: testUserId,
         name: 'Cello practice',
         createdAt: new Date('2023-01-15T00:00:00.000Z'),
         schedule: SCHEDULE.MONING,
@@ -123,7 +146,8 @@ async function main() {
         date: new Date('2023-01-17T00:00:00.000Z'),
         dayHabits: {
           create: {
-            habit_id: firstHabitId
+            habit_id: firstHabitId,
+            user_id: testUserId,
           }
         }
       }
@@ -133,8 +157,8 @@ async function main() {
         date: new Date('2023-01-20T00:00:00.000Z'),
         dayHabits: {
           create: [
-            { habit_id: firstHabitId },
-            { habit_id: secondHabitId }
+            { habit_id: firstHabitId, user_id: testUserId, },
+            { habit_id: secondHabitId, user_id: testUserId, }
           ]
         }
       }
@@ -144,7 +168,8 @@ async function main() {
   await Promise.all([
     prisma.task.create({
       data: {
-        name: 'Limpar geladeira',
+        user_id: testUserId,
+        name: 'buy some flowers',
         target_date: dateToMidnightISODate(addDays(new Date(), 2)),
         priority: 1,
         createdAt: dateToMidnightISODate(new Date()),
@@ -153,7 +178,8 @@ async function main() {
     }),
     prisma.task.create({
       data: {
-        name: 'Carpir o fundo',
+        user_id: testUserId,
+        name: 'Prepare to restaurant',
         target_date: dateToMidnightISODate(addDays(new Date(), 1)),
         priority: 1,
         createdAt: dateToMidnightISODate(new Date()),
@@ -162,7 +188,8 @@ async function main() {
     }),
     prisma.task.create({
       data: {
-        name: 'Separar roupa para parto',
+        user_id: testUserId,
+        name: 'Schedule XPTO call',
         target_date: dateToMidnightISODate(addDays(new Date(), 1)),
         priority: 0,
         createdAt: dateToMidnightISODate(new Date()),
